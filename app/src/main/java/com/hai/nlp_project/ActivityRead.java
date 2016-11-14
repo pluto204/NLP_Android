8b3e9dd2068af419532ce85c46d82c386ce869b8
pluto204 <pluto204@users.noreply.github.com>
2016-11-14 13:03:46 +0700

    Update ActivityRead.java

--- app/src/main/java/com/hai/nlp_project/ActivityRead.java
+++ app/src/main/java/com/hai/nlp_project/ActivityRead.java
@@ -13,11 +13,14 @@ import com.tom_roush.pdfbox.text.PDFTextStripperByArea;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ActivityRead extends AppCompatActivity {
 
     TextView textView;
     static String fileName;
+    static final String ALPHABET = "!?(),.";
+    static Scanner sc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +31,14 @@ public class ActivityRead extends AppCompatActivity {
         String message = intent.getStringExtra("filename");
         File root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
         fileName = root.getPath() + "/Download/" + message + ".pdf";
-
         Toast.makeText(ActivityRead.this, message, Toast.LENGTH_SHORT).show();
         textView= (TextView)findViewById(R.id.content);
-        textView.setText(readAll());
+        String s = readAll();
+        sc = new Scanner(s);
+        textView.setText(s);
     }
 
+    //doc file pdf
     public static String readAll() {
         String content = "";
         try {
@@ -51,5 +56,40 @@ public class ActivityRead extends AppCompatActivity {
             e.printStackTrace();
         }
         return content;
+    }
+
+    //Hàm này sử dụng cho xử lý tìm lỗi sai, trả về numOfWord từ tiếp theo của các từ đã đọc.
+    public static String[] readPart(int numOfWord) {
+        String[] result = new String[numOfWord];
+        for (int i = 0; i < numOfWord; i++) {
+            if(sc.hasNext()){
+                result[i] = repairWord(sc.next());
+            }
+        }
+        sc.close();
+        return result;
+    }
+
+    static String repairWord(String word){
+        word = word.toLowerCase();
+        int n = word.length();
+        for(int i = n-1; i>=0; i--){
+            if(checkChar(word.charAt(i))){
+                word = deleteChar(word, i);
+            }
+        }
+        return word;
+    }
+
+    static String deleteChar(String word, int index){
+        return word.substring(0, index) + word.substring(index+1);
+    }
+
+    static boolean checkChar(char c){
+        int n = ALPHABET.length();
+        for(int i= 0; i<n; i++){
+            if(c == ALPHABET.charAt(i)) return true;
+        }
+        return false;
     }
 }
