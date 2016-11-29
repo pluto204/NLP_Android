6d0b6fc6aabbd9e54c35e2abffe5c079ea01447f
pluto204 <minhhaihustk57@gmail.com>
2016-11-29 11:08:03 +0700

    fix conflict

--- app/src/main/java/com/hai/nlp_project/ActivityRead.java
+++ app/src/main/java/com/hai/nlp_project/ActivityRead.java
@@ -16,26 +16,19 @@ import com.tom_roush.pdfbox.text.PDFTextStripperByArea;
 
 import java.io.File;
 import java.io.IOException;
-<<<<<<< HEAD
 import java.io.InputStream;
-=======
->>>>>>> refs/remotes/origin/NLP
 import java.util.Scanner;
 
 public class ActivityRead extends AppCompatActivity {
 
     TextView textView,errorTextView;
     static String fileName;
-<<<<<<< HEAD
     SyllableTree st;
-
-    static final String ALPHABET = "!?(),.";
+    
     String fileContent;
     int errorCount = 0;
-=======
     static final String ALPHABET = "!?(),.";
     static Scanner sc;
->>>>>>> refs/remotes/origin/NLP
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +41,6 @@ public class ActivityRead extends AppCompatActivity {
         fileName = root.getPath() + "/Download/" + message + ".pdf";
         Toast.makeText(ActivityRead.this, message, Toast.LENGTH_SHORT).show();
         textView= (TextView)findViewById(R.id.content);
-<<<<<<< HEAD
         errorTextView= (TextView)findViewById(R.id.error);
         fileContent = readAll();
         textView.setText(fileContent);
@@ -75,11 +67,9 @@ public class ActivityRead extends AppCompatActivity {
                 errorTextView.setText(result);
             }
         });
-=======
         String s = readAll();
         sc = new Scanner(s);
         textView.setText(s);
->>>>>>> refs/remotes/origin/NLP
     }
 
     //doc file pdf
@@ -102,8 +92,6 @@ public class ActivityRead extends AppCompatActivity {
         return content;
     }
 
-<<<<<<< HEAD
-=======
     //Hàm này sử dụng cho xử lý tìm lỗi sai, trả về numOfWord từ tiếp theo của các từ đã đọc.
     public static String[] readPart(int numOfWord) {
         String[] result = new String[numOfWord];
@@ -116,7 +104,6 @@ public class ActivityRead extends AppCompatActivity {
         return result;
     }
 
->>>>>>> refs/remotes/origin/NLP
     static String repairWord(String word){
         word = word.toLowerCase();
         int n = word.length();
@@ -139,7 +126,6 @@ public class ActivityRead extends AppCompatActivity {
         }
         return false;
     }
-<<<<<<< HEAD
 
     String readFromFile(Context context, String file) {
         try {
@@ -156,6 +142,3 @@ public class ActivityRead extends AppCompatActivity {
     }
 
 }
-=======
-}
->>>>>>> refs/remotes/origin/NLP
